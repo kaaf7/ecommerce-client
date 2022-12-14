5cee291acce8fb773c7357c93a1ee6fb363beb83
kaaf7 <amer.m.karim@gmail.com>
2022-12-14 13:50:09 +0100

    edit base URL

--- src/services.js
+++ src/services.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:3005/api";
+const BASE_URL = "https://ecomm-backend-server.onrender.com/api";
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 const currentUser = user && JSON.parse(user).currentUser;
 const TOKEN = currentUser?.accessToken;
