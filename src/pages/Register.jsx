009a8917afa41b4ad257f35a28afca67154dae10
kaaf7 <amer.m.karim@gmail.com>
2022-12-16 20:20:36 +0100

    edit registeration error

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -126,9 +126,9 @@ const Register = () => {
             By creating an account, I consent to the processing of my personal
             data in accordance with the <b>PRIVACY POLICY</b>
           </Agreement>
+          {error && <ErrorText>WRONG CREDENTIALS</ErrorText>}
           <Button onClick={handleRegister}>CREATE</Button>
         </Form>
-        {error && <ErrorText>WRONG CREDENTIALS</ErrorText>}
       </Wrapper>
     </Container>
   );
