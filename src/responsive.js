e06a43fde9685081d41ce8b57b955d4fa4c714df
kaaf7 <amer.m.karim@gmail.com>
2022-12-24 14:38:39 +0100

    add mcBook responsive

--- src/responsive.js
+++ src/responsive.js
@@ -2,7 +2,7 @@ import { css } from "styled-components";
 
 export const mobile = (props) => {
   return css`
-    @media only screen and (max-width: 415px) {
+    @media only screen and (max-width: 428px) {
       ${props}
     }
   `;
@@ -10,7 +10,23 @@ export const mobile = (props) => {
 
 export const tablet = (props) => {
   return css`
-    @media only screen and (min-width: 415px) and (max-width: 1023px) {
+    @media only screen and (min-width: 415px) and (max-width: 1025px) {
+      ${props}
+    }
+  `;
+};
+
+export const mcBook = (props) => {
+  return css`
+    @media only screen and (min-width: 1025px) and (max-width: 1919px) {
+      ${props}
+    }
+  `;
+};
+
+export const macBookro = (props) => {
+  return css`
+    @media only screen and (min-width: 1920px) and (max-width: 2560px) {
       ${props}
     }
   `;
